8975e7863603959e083fb6834926b862eddc992f
Ashutosh Vaishnav <avaishnav6292@gmail.com>
2021-09-17 00:12:25 +0530

    Update dynamodbConfig.js

--- backend/src/dynamodbConfig.js
+++ backend/src/dynamodbConfig.js
@@ -2,7 +2,7 @@ var AWS = require("aws-sdk");
 let awsConfig = {
     "region": "ap-south-1",
     "endpoint": "http://dynamodb.ap-south-1.amazonaws.com",
-    "accessKeyId": "AKIA56ITW6S4I3DWZBSX", "secretAccessKey": "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"
+    "accessKeyId": "ACCESS_KEY_ID", "secretAccessKey": "SECRET_KEY_ID"
 };
 AWS.config.update(awsConfig);
 
